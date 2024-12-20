005c75f7ade79f05ebbd806240ef77363ee4aadc
Davide Gonnelli <davide.gonnelli1@gmail.com>
2024-12-20 01:15:06 +0100

    stampo immagini membri

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "female3.png"
   }
 ];
 
@@ -47,11 +47,11 @@ for(let i = 0; i < teamMembers.length; i++) {
   listItems += `
    <div class="team-card">
                 <div class="card-image">
-                        <img src="img/male1.png" alt="Mario Bianchi">
+                        <img src="img/${member.img}" alt="Mario Bianchi">
                     </div>
                     <div class="card-text">
-                        <h3>Mario Bianchi</h3>
-                        <p>Designer</p>
+                        <h3>${member.name}</h3>
+                        <p>${member.role}</p>
                     </div>
                 </div>
   `; 
